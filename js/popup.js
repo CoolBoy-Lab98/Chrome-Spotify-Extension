4cf0a87f9907398ad87d4af84e57bc7d822afa05
CoolBoy-Lab98 <myself986532@gmail.com>
2025-03-19 13:35:49 +0530

    Add initial JavaScript for popup functionality

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const loginButton = document.getElementById('login-button');
+  
+  loginButton.addEventListener('click', function() {
+    // TODO: Implement Spotify OAuth login
+    console.log('Login button clicked');
+  });
+});
